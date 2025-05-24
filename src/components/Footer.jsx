50fe4ffbb590b67861b4e168d6b469dbfee31977
prasanthoyasco <prasanthoyasco@gmai.com>
2025-05-24 20:01:21 +0530

    delte btn enabled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,7 @@ import GoldRatesDisplay from "./GoldRatesDisplay"
 export const Footer = () => {
   return (
     <footer
-      className="bg-white border-top py-3 px-3 d-flex flex-column flex-md-row justify-content-between align-items-center"
-      style={{ paddingLeft: "1rem", paddingRight: "1rem",marginLeft:'240px' }}
+      className="bg-white border-top py-3 px-3 d-flex flex-column flex-md-row justify-content-between align-items-center footer"
     >
       <GoldRatesDisplay />
       <small className="text-muted text-center text-md-end mt-2 mt-md-0 d-none d-md-block">
